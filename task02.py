ac5ed8a323a6b10973fe0a72ea5abe21a2cd402f
도형 홍 <begace11@gmail.com>
2023-10-23 12:33:06 +0900

    task02 : 가져온 링크 txt 파일을 순회하며 title과 review를 csv로 저장함 + 더보기 버튼 예외처리 + 더보기 버튼 추가 예외처리 + 제목 예외처리 + 존재하지 않는 페이지 예외처리

--- task02.py
+++ task02.py
@@ -14,6 +14,7 @@ import time
 def scrape_reviews():
     driver = webdriver.Chrome()  # chromedriver가 PATH에 있는지 확인하세요
     data = []
+    ifsuc = True
 
     with open('addresses.txt', 'r') as file:
         urls = file.readlines()
@@ -30,45 +31,48 @@ def scrape_reviews():
             # 제목 가져오기
             title_xpath = '//*[@id="mainContent"]/div/div[1]/div[2]/div[1]/h3/span[1]'
             title = driver.find_element(By.XPATH, title_xpath).text
+            ifsuc = True
         except Exception as e:
             with open('errors.txt', 'a') as error_file:
                 error_file.write(f'Error at URL {url.strip()}: Could not find the title.\n')
+            ifsuc = False
             continue  # 제목을 찾을 수 없으면 다음 URL로 넘어감
 
-        # 4번째 탭에서 URL을 가져와서 이동
-        tab_xpath = '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a'
-        tab_url = driver.find_element(By.XPATH, tab_xpath).get_attribute('href')
-        driver.get(tab_url)
+        if ifsuc == True:
+            # 4번째 탭에서 URL을 가져와서 이동
+            tab_xpath = '//*[@id="mainContent"]/div/div[2]/div[1]/ul/li[4]/a'
+            tab_url = driver.find_element(By.XPATH, tab_xpath).get_attribute('href')
+            driver.get(tab_url)
 
-        try:
-            # "더 보기" 버튼이 표시될 때까지 대기
-            show_more_xpath = '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'
-            WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, show_more_xpath))
-            )
-        except Exception as e:
-            print(f'Error at URL {url.strip()}: Could not find the "Show more" button.')
-            print(str(e))
-            continue  # 오류가 발생하면 다음 URL로 넘어감
+            try:
+                # "더 보기" 버튼이 표시될 때까지 대기
+                show_more_xpath = '//*[@id="alex-area"]/div/div/div/div[3]/div[1]/button'
+                WebDriverWait(driver, 10).until(
+                    EC.visibility_of_element_located((By.XPATH, show_more_xpath))
+                )
+            except Exception as e:
+                print(f'Error at URL {url.strip()}: Could not find the "Show more" button.')
+                print(str(e))
+                continue  # 오류가 발생하면 다음 URL로 넘어감
 
-        # "더 보기" 버튼을 5번 클릭하여 더 많은 리뷰를 로드
-        clicked_times = 0
-        try:
-            for _ in range(5):
-                driver.find_element(By.XPATH, show_more_xpath).click()
-                clicked_times += 1
-                time.sleep(2)  # 콘텐츠가 로드되는데 시간을 주기 위해 지연 추가
-        except Exception as e:
-            print(f'Error at URL {url.strip()}: Clicked {clicked_times} times before an error occurred.')
-            print(str(e))
+            # "더 보기" 버튼을 5번 클릭하여 더 많은 리뷰를 로드
+            clicked_times = 0
+            try:
+                for _ in range(5):
+                    driver.find_element(By.XPATH, show_more_xpath).click()
+                    clicked_times += 1
+                    time.sleep(2)  # 콘텐츠가 로드되는데 시간을 주기 위해 지연 추가
+            except Exception as e:
+                print(f'Error at URL {url.strip()}: Clicked {clicked_times} times before an error occurred.')
+                print(str(e))
 
-        # start_xpath에서 end_xpath까지 리뷰 스크랩
-        for i in range(1, 161):  # 160개의 리뷰가 로드되었다고 가정
-            review_xpath = f'/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{i}]/div/p'
-            elements = driver.find_elements(By.XPATH, review_xpath)
-            if elements:  # 요소가 존재하면
-                review_text = elements[0].text.replace('\n', ' ')  # 줄바꿈 제거
-                data.append({'Title': title, 'Review': review_text})
+            # start_xpath에서 end_xpath까지 리뷰 스크랩
+            for i in range(1, 161):  # 160개의 리뷰가 로드되었다고 가정
+                review_xpath = f'/html/body/div[2]/main/article/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[3]/ul[2]/li[{i}]/div/p'
+                elements = driver.find_elements(By.XPATH, review_xpath)
+                if elements:  # 요소가 존재하면
+                    review_text = elements[0].text.replace('\n', ' ')  # 줄바꿈 제거
+                    data.append({'Title': title, 'Review': review_text})
 
     # 데이터를 CSV 파일에 저장
     df = pd.DataFrame(data)
